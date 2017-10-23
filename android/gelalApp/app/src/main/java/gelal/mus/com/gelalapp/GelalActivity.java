3b808c4ec0e4258d8c226048f8ea54f3195f0745
MrDisarm0 <disarm0@nate.com>
2017-10-23 15:01:10 +0900

    first

---
+++ android/gelalApp/app/src/main/java/gelal/mus/com/gelalapp/GelalActivity.java
@@ -0,0 +1,13 @@
+package gelal.mus.com.gelalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GelalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gelal);
+    }
+}
